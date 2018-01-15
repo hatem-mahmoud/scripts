2b9302d671a1a776c172106351de77f432e0e46e
Hatem Mahmoud <h.mahmoud87@gmail.com>
2018-01-15 14:25:46 +0100

    Update sql_resolver.sh

--- sql_resolver.sh
+++ sql_resolver.sh
@@ -3,6 +3,7 @@ offset_var=0x`echo $1 | cut -d: -f2`
 kglhd_var=0x`echo $1 | cut -d: -f3`
 padding=`echo $1 | cut -d: -f4`
 
+# ARGUMENT 1 and 3 of funtion pfrln0lookup  have to be investigated in more detail for now i just put any address that s mapped to VAS of the target process
 line_var=`sqlplus / as sysdba <<EOF 2>&1 | grep -i function | cut -d' ' -f4
 oradebug setmypid
 oradebug call pfrln0lookup 0x7ffff3c6bc38 $kglhd_var 0x7ffff3c6bc38 $offset_var
